a9fbc0c0abfa8671cf77150e384eb221101d1efc
iswayanabisma@gmail.com <BismaAjil@email.com>
2019-12-17 14:53:45 +0700

    menambah Detail Activity.java

--- app/src/main/java/id/ac/polinema/beliphone/activities/DetailActivity.java
+++ app/src/main/java/id/ac/polinema/beliphone/activities/DetailActivity.java
@@ -2,10 +2,14 @@ package id.ac.polinema.beliphone.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -14,16 +18,26 @@ import id.ac.polinema.beliphone.R;
 public class DetailActivity extends AppCompatActivity {
     TextView title,desc;
     ImageView imageView2;
+    Button beli;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         title = findViewById(R.id.title);
         desc = findViewById(R.id.desc);
+        beli = findViewById(R.id.beli);
         imageView2 = findViewById(R.id.imageView2);
         title.setText(getIntent().getExtras().getString("title"));
         desc.setText(getIntent().getExtras().getString("desc"));
-
+        beli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Pembelian Berhasil",
+                        Toast.LENGTH_LONG).show();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
         if(getIntent().getExtras().getString("code_image").equals("1")){
             Glide
                     .with(this)
